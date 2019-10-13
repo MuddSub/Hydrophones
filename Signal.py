f699ca09faab1792ce771a113f4b156110f70774
occamkg <okellygraves@hmc.edu>
2019-10-13 01:51:42 -0700

    Added pings and x-y sensor position

--- Signal.py
+++ Signal.py
@@ -1,61 +1,75 @@
-# Occam Kelly Graves
-# 2019-09-15
+#Occam Kelly Graves
+#2019-10-13
 import math
 import numpy
 import random
 
-ARRAY_SEPARATION = 0.01 # distance between centers of sensors (m)
-NUM_SENSORS = 4
+###calculate the distance to sensor based on ref point
+#coords: [x, y] distances in m from ref point
+#distance: distance to ref point
+#angle: between x-axis and ping to ref point
+def calc_distance(coords, distance, angle):
+    x_source = distance * math.cos(angle)
+    y_source = distance * math.sin(angle)
+    return math.sqrt((coords[0] - x_source)**2 + (coords[1] - y_source)**2)
 
-def calc_distance(sensor_num, side1, angle):
-    side2 = abs(1.5 - sensor_num) * ARRAY_SEPARATION
-    if(sensor_num < 1.5):
-        angle = math.pi - angle
-    return math.sqrt(side1**2 + side2**2 - 2 * side1 * side2 * math.cos(angle))
-
+###calculate a single reading of the sensor (pure sine wave)
+#distance: distance from source to sensor (m)
+#time: current reading time (ms)
+#frequency: ping frequency (kHz)
+#sound_speed: current speed of sound (m/ms)
+#amplitude: amplitude of sine wave
 def calc_sine(distance, time, frequency, sound_speed, amplitude):
     tot_time = distance / sound_speed + time
     return amplitude * math.sin(2 * math.pi * frequency * tot_time)
 
-def add_noise(noise, signal):
-    noise_array = numpy.random.normal(0, noise, len(signal))
-    noise_signal = []
-    for i in range(len(signal)):
-        noise_signal.append(signal[i] + noise_array[i])
-    return noise_signal
-
 class Signal:
-    #                        source
-    #                         /
-    #              distance /
-    #                     /
-    #                   /
-    #                 / angle
-    # sensors  *   *   *   *
-    #   index: 0   1   2   3
-    def __init__(self, distance, angle, frequency, sound_speed, sample_rate, read_time, noise):
-        self.distance = distance #m
+    ###initializer
+    def __init__(self, distance, angle, coords, frequency, sound_speed, sample_rate, read_time, noise, time_on, ping_period, ping_start):
+        self.distance = calc_distance(coords, distance, angle) #m
         self.angle = angle #rad between 0 and pi
         self.frequency = frequency #kHz
         self.sound_speed = sound_speed #m/ms
         self.sample_rate = sample_rate #kHz
         self.read_time = read_time #ms
         self.noise = noise #standard deviation
+        self.time_on = time_on #ms
+        self.ping_period = ping_period #ms
+        self.ping_start = ping_start #ms
 
         self.signals = self.generate_signals()
 
+    ###generate complete simulated signal reading array for the sensor
     def generate_signals(self):
-        signals = []
-        for i in range(4):
-            signal = []
-            distance = calc_distance(i, self.distance, self.angle)
-            echo_length = random.random() * 100
-            echo_amp = random.uniform(0.7, 0.9)
-            for j in range(math.floor(self.sample_rate * self.read_time)):
-                sig = calc_sine(distance, j / self.sample_rate, self.frequency,
-                  self.sound_speed, 1)
-                echo_sig = calc_sine(distance + echo_length, j / self.sample_rate,
-                  self.frequency, self.sound_speed, echo_amp)
-                signal.append(sig) #+ echo_sig)
-            signals.append(add_noise(self.noise, signal))
-        return signals
+        signal = []
+        #determine the start time of the first ping
+        start = self.ping_start + self.distance / self.sound_speed
+        if(start > self.ping_period - self.time_on):
+            start -= self.ping_period
+
+        #create echo parameters for random echo
+        echo_length = random.random() * 100
+        echo_amp = random.uniform(0.3, 0.5)
+        echo_freq = random.uniform(0.9, 1.1) * self.frequency
+        #determine the start time of the echo's first ping
+        echo_start = self.ping_start + self.distance + echo_length / self.sound_speed
+        if(echo_start > self.ping_period - self.time_on):
+            echo_start -= self.ping_period
+
+        #calculate the total number of samples for the sensor
+        samples = math.floor(self.sample_rate * self.read_time)
+        #create a noise array to add to the pings
+        noise_array = numpy.random.normal(0, self.noise, samples)
+        #calculate each ping reading with echoes
+        for i in range(samples):
+            sig = 0
+            #add ping sine wave if sensor is recieving a ping
+            if(((i / self.sample_rate - start) % self.ping_period) < self.time_on):
+                sig += calc_sine(self.distance, i / self.sample_rate, self.frequency, self.sound_speed, 1)
+            #add echo sine wave if sensor is recieving an echo
+            if(((i / self.sample_rate - echo_start) % self.ping_period) < self.time_on):
+                sig += calc_sine(self.distance + echo_length, i / self.sample_rate, echo_freq, self.sound_speed, echo_amp)
+            #add noise
+            signal.append(sig + noise_array[i])
+
+        return signal
--- TestSignal.py
+++ TestSignal.py
@@ -1,7 +1,10 @@
+#Occam Kelly Graves
+#2019-10-13
 import Signal
 
 import matplotlib.pyplot as plt
 import math
+import random
 import numpy
 
 def graph_signals(time, array):
@@ -14,16 +17,21 @@ def graph_signals(time, array):
 def main():
     d = 20 #m
     angle = 1.8 #rad
-    freq = 30 #kHz
+    coords = [[0.005, 0.005], [0.005, -0.005], [-0.005, -0.005], [-0.005, 0.005]]
+    freq = 45 #kHz
     sos = 1.5 #m/ms
     sr = 1000 #kHz
-    read = 0.1 #ms
+    read = 1500 #ms
     noise = 1 #standard deviation
-    signals = Signal.Signal(d, angle, freq, sos, sr, read, noise).signals
-    times = numpy.arange(0, read, 1/sr)
+    time_on = 100 #ms --ping duration
+    ping_period = 1000 #ms
+    ping_start = random.random() * ping_period
 
-    for sig in signals:
-        print(sig)
+    #create an array containing the simulated readings arrays of each sensor
+    signals = []
+    for i in range(len(coords)):
+        signals.append(Signal.Signal(d, angle, coords[i], freq, sos, sr, read, noise, time_on, ping_period, ping_start).signals)
+    times = numpy.arange(0, read, 1/sr)
 
     graph_signals(times, signals)
 
