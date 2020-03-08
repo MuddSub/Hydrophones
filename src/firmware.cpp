6f045ab9ae2cfc7cec0e56c33324fd61e40e6e92
sethgi <sisaacson@g.hmc.edu>
2020-03-08 09:15:12 +0000

    refactor

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -5,31 +5,56 @@ Hydrophones::Hydrophones(){
 	return;
 }
 
-//translated from wikipedia python ex
-char Hydrophones::calcCRC(reg data){
-	int array[24];
-	for(i = 0; i < 16; ++i)
-		array[i] = (data >> i)&1;
-	for(i = 16; i < 24; ++i)
-		array[i] = 0;
 
-	//x^16 + x^15 + x^12 + x^7 + x^6 + x^4 + x^3 + 1
-	int polynomial[17] = {1,1,0,0,1,0,0,0,0,1,1,0,1,1,0,0,1};
-
-	while(true){
-		bool oneFound = false;
-		for(auto i : array){
-			if i != 0:
-				oneFound = true;
-				break;
-		if(!oneFound) break;
-		
-
-
-	}
-
+word Hydrophones::spiRead(word addr){
+	unsigned char* buffer[3];
+	buffer[0] = addr15*2; //set bit 1 to 1 or 0 accordingly, bit 0 is always 0
+	buffer[1] = addr >> 8;     //shift to get only MSB
+	buffer[2] = (addr & 0xFF); //mask MSB with zeros
+	int err = wiringPiSPIDataRW(channel, buffer, 3);
+	word result = buffer[0];
+	result |= (buffer[1] << 8);
+	return result;
 }
 
+void Hydrophones::spiWrite(word addr, std::vector<word> data){
+	unsigned char* buffer[maxWrite*2 + 1];
+	int len = 2*data.size() + 3;
+	buffer[0] = 0x1;
+	buffer[1] = addr >> 8;     //shift to get only MSB
+	buffer[2] = (addr & 0xFF); //mask MSB with zeros
+	for(int i = 0; i < data.size(); ++i){
+		buffer[i+3] = data[i] >> 8;     //shift to get only MSB
+		buffer[i+4] = (data[i] & 0xFF); //mask MSB with zeros
+	}
+	int err = wiringPiSPIDataRW(channel, buffer, len);
+}
+
+void Hydrophones::spiWrite(word addr, word data){
+	std::vector<word> v;
+	v.push_back(data);
+	spiWrite(addr, v);
+}
+
+void Hydrophones::setBit(word& data, int pos, bool value){
+	word mask = (1 << pos);
+	if(value == 1){
+		data |= mask;
+		return;
+	}
+	//invert the mask
+	mask = ~mask;
+	data &= mask;
+}
+
+void Hydrophones::setBits(word& data, word mask, bool value){
+	if(value == 1){
+		data |= mask;
+		return;
+	}
+	mask = ~mask;
+	data &= mask;
+}
 
 
 int main(){
